f710c859f9ca2126740ee5706597e4174fed707d
Shawn Chen <xcbh6@mail.missouri.edu>
2022-01-15 17:28:09 -0600

    add multiprocess_toil

---
+++ handy_tools/multiprocess_tool.py
@@ -0,0 +1,23 @@
+"""
+@ Description: Wrapper for multiprocessing
+@ Author: Shawn Chen
+@ Date: 2022-01-15
+"""
+
+
+import multiprocessing as mp
+
+
+def mp_wrapper(func, args_list, threads=-1):
+    if threads == -1:
+        threads = mp.cpu_count()
+    
+    p = mp.Pool(threads)
+    for args in args_list:
+        p.apply_async(func, args)
+    
+    p.close()
+    p.join()
+    print('Finished')
+
+    return None
