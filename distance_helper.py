ce5907c91da8ff7a578b9cc09dcbef90b7c20c0d
Shawn Chen <kingchen92@gmail.com>
2022-02-23 16:40:47 -0600

    new function

--- distance_helper.py
+++ distance_helper.py
@@ -48,6 +48,16 @@ def distance_helper(pdb_file: str, pdb_name: str, output_folder: str, atom_type=
         real_dist = euclidean_distances(coord_N, coord_O)  # up-triangle N-O, low-triangle O-N
         np.fill_diagonal(real_dist, 0)  # set diagnoal value to 0
     
+    # # get full N-O distance map and O-N distance map
+    # e_1 = np.identity(real_dist.shape[0])[:,::1]
+    # no_distance = np.triu(real_dist)
+    # no_mirror = e_1.dot(no_distance.T).dot(e_1)
+    # no_full = no_mirror + no_distance
+
+    # on_distance = np.tril(real_dist)
+    # on_mirror = e_1.dot(on_distance.T).dot(e_1)
+    # on_full = on_mirror + on_distance
+    
     real_dist = np.round(real_dist, 3)
 
 
