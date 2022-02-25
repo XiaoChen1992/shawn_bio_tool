3406d99ece0fed8ba7a5bfcdd2227a7a76e751bc
Shawn Chen <xcbh6@mail.missouri.edu>
2022-02-25 16:25:33 -0600

    Update dockq_score.py

--- dockq_score.py
+++ dockq_score.py
@@ -8,8 +8,8 @@
 def calculate_dockq(fnat: float, lrms: float, irms: float) -> float:
 	def scaled(rms: float, d: float) -> float:
 		return 1 / (1 + (rms / d)**2)
-	scaled_lrms = scaled(lrms, d1)
-	scaled_irms = scaled(irms, d2)
+	scaled_lrms = scaled(lrms, d=8.5)
+	scaled_irms = scaled(irms, d=1.5)
 
 	return (fnat, scaled_lrms + scaled_irms) / 3
 
