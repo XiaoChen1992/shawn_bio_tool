b84f22e1da6f1acb69a522ddc181b02645af735a
Shawn Chen <xcbh6@mail.missouri.edu>
2022-02-25 16:02:44 -0600

    Update dockq_score.py

--- dockq_score.py
+++ dockq_score.py
@@ -1,5 +1,5 @@
 """
-@ Description: Generate DockQ score by Fnat, LRMS, iRMS
+@ Description: Generate DockQ score by Fnat, LRMS, iRMS, map dockq to CAPRI standard
 @ Author: Shawn Chen
 @ Date: 2022-02-25
 """
@@ -12,3 +12,17 @@ def calculate_dockq(fnat: float, lrms: float, irms: float) -> float:
 	scaled_irms = scaled(irms, d2)
 
 	return (fnat, scaled_lrms + scaled_irms) / 3
+
+
+def classification(dockq_score: float) -> int:
+	"""0: incorrect, 1: acceptable, 2: medium, 3: high"""
+    if 0 <= dockq_score < 0.23:
+        return 0
+    elif 0.23 <= dockq_score < 0.49:
+        return 1
+    elif 0.49 <= dockq_score < 0.8:
+        return 2
+    elif dockq_score >= 0.8:
+        return 3
+    else:
+        raise ValueError(f'The dockq score shout between 0 and 1. The current value is {dockq_score}')
